052c712d6ef40335b4d38f28655feef31d871d56
code2practice <tiknatim@gmail.com>
2025-02-18 00:47:03 -0800

    Update 215. Kth Largest Element in an Array.py

--- Solutions/215. Kth Largest Element in an Array.py
+++ Solutions/215. Kth Largest Element in an Array.py
@@ -12,7 +12,15 @@ Example 2:
 Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
 Output: 4
 '''
+
+'''
+1. First, we need to choose so-calledpivotand partition element ofnumsinto 3 parts: elements, smaller than pivot, equal to pivot and bigger than pivot. (sometimes two groups enough: less and more or equal)
+2. Next step is to see how many elements we have in each group: ifk <= L, whereLis size ofleft, than we can be sure that we need to look into the left part. Ifk > L + M, whereMis size ofmidgroup, than we can be sure, that we need to look into the right part. Finally, if none of these two condition holds, we need to look in themidpart, but because all elements there are equal, we can immedietly returnmid[0].
 # (Important solution)RunTime O(N)
+
+Complexity: time complexity isO(n)in average, because on each time we reduce searching range approximately2times. This is not strict proof, for more details you can do some googling. Space complexity isO(n)as well.
+'''
+
 class Solution:
    def findKthLargest(self, nums: List[int], k: int) -> int:
        pivot = random.choice(nums)

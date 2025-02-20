963ae5c012e005d32177ab65bf1619f82f265297
code2practice <tiknatim@gmail.com>
2025-02-19 18:17:32 -0800

    Create 241. Different Ways to Add Parentheses.py

---
+++ Solutions/241. Different Ways to Add Parentheses.py
@@ -0,0 +1,50 @@
+'''
+241. Different Ways to Add Parentheses
+Medium
+Given a string expression of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. You may return the answer in any order.
+The test cases are generated such that the output values fit in a 32-bit integer and the number of different results does not exceed 104.
+ 
+Example 1:
+Input: expression = "2-1-1"
+Output: [0,2]
+Explanation:
+((2-1)-1) = 0 
+(2-(1-1)) = 2
+Example 2:
+Input: expression = "2*3-4*5"
+Output: [-34,-14,-10,-10,10]
+Explanation:
+(2*(3-(4*5))) = -34 
+((2*3)-(4*5)) = -14 
+((2*(3-4))*5) = -10 
+(2*((3-4)*5)) = -10 
+(((2*3)-4)*5) = 10
+ 
+Constraints:
+1 <= expression.length <= 20
+expression consists of digits and the operator '+', '-', and '*'.
+All the integer values in the input expression are in the range [0, 99].
+'''
+
+class Solution(object):
+    def diffWaysToCompute(self, input):
+        return self.dfs(input)
+    
+    @cache
+    def dfs(self, input):
+        if input.isdigit():
+            return [int(input)]
+        res = []
+        for i, c in enumerate(input):
+            if c in "+-*":
+                l = self.diffWaysToCompute(input[:i])
+                r = self.diffWaysToCompute(input[i+1:])
+                for left_v in l:
+                    for right_v in r:
+                        if c == '+':
+                            res.append(left_v + right_v)
+                        elif c == '-':
+                            res.append(left_v - right_v)
+                        else:
+                            res.append(left_v * right_v)
+        return res
